2924714d77819f76b0beca22a4d192077a9d2426
Adam Braus <ajbraus@gmail.com>
2020-04-21 13:33:49 -0700

    Initial Commit

---
+++ RoboDonor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RoboDonor
+//
+//  Created by Adam Braus on 4/21/20.
+//  Copyright © 2020 Adam Braus. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
